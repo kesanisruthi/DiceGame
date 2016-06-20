d75850ba041a3c0ed955b8a9dd9d4a410a10c495
kesan <kesan@LAPTOP-T316MMMD>
2016-06-20 15:25:24 -0500

    Taking counters for implementing logic

--- Dice/src/com/iquity/DiceGame.java
+++ Dice/src/com/iquity/DiceGame.java
@@ -1,9 +1,16 @@
 package com.iquity;
 
 public class DiceGame {
+	public static int count_1=0;
+	public static int count_2=0;
+	public static int count_3=0;
+	public static int count_4=0;
+	public static int count_5=0;
+	public static int count_6=0;
+	public static int count_invalid=0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int input[]={5,4,0,4,5};
 
 	}
 

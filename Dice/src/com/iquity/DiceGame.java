d5c3840becce4ba046d12f5d0b9e3a9e4932389a
kesan <kesan@LAPTOP-T316MMMD>
2016-06-20 15:44:50 -0500

    Calculating points

--- Dice/src/com/iquity/DiceGame.java
+++ Dice/src/com/iquity/DiceGame.java
@@ -29,6 +29,41 @@ public class DiceGame {
 		} 
 		
 	}
+	public static int calculatePoints(){
+		int points=0;
+		if(count_invalid>0){
+			points=0;
+		}else {
+			if(count_1==3){
+				points=1000;
+			}else if(count_1==2){
+				points+=200;
+			} else if(count_1==1){
+				points+=100;
+			}
+			
+			if(count_2==3){
+				points+=200;
+			}
+			if(count_3==3){
+				points+=300;
+			}
+			if(count_4==3){
+				points+=400;
+			}else if(count_4==2){
+				points+=80;
+			} else if(count_4==1){
+				points+=40;
+			}
+			if(count_5==3){
+				points+=500;
+			}
+			if(count_6==3){
+				points+=600;
+			}
+		}
+		return points;
+	}
 
 	public static void main(String[] args) {
 		int input[]={5,4,0,4,5};
